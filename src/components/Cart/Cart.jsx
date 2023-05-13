0f6d80c46b918c51f3741c8687be743ba4b889f4
Mahmud Hasan <mahmud.hasan2006@gmail.com>
2023-05-13 12:37:19 +0600

    cart add & claculation done with local storage

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,20 +3,27 @@ import './Cart.css'
 
 const Cart = ({ cart }) => {
     console.log(cart);
-    let total = 0;
+    let totalPrice = 0;
     let totalShipping = 0;
+    let quantity = 0;
     for (const product of cart) {
-        total = total + product.price;
+        // if (product.quantity === 0) {
+        //     product.quantity = 1;
+        // }
+        totalPrice = totalPrice + product.price * product.quantity;
         totalShipping = totalShipping + product.shipping;
+        quantity = quantity + product.quantity;
     }
-    const tax = total * 7 / 100;
-    const grandTotal = total + totalShipping + tax;
+    const tax = totalPrice * 7 / 100;
+
+    const grandTotal = totalPrice + totalShipping + tax;
+
     return (
         <div className='cart'>
             <h3>Order Summary</h3>
-            <p>Selected Items: {cart.length}</p>
-            <p>Total Price: $ {total} </p>
-            <p>Total Shipping:{totalShipping} </p>
+            <p>Selected Items: {quantity}</p>
+            <p>Total Price: $ {totalPrice} </p>
+            <p>Total Shipping:${totalShipping} </p>
             <p>Tax: $ {tax}</p>
             <h6>Grand Total: {grandTotal}</h6>
         </div>
